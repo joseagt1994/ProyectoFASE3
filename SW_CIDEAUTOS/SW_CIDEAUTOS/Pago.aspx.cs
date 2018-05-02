aca718b0fa68fb21a5eedca61936931ca9fb489f
Luis Garcia <luis13mzm@gmail.com>
2018-05-01 19:04:20 -0600

    Pagos.aspx agregado

---
+++ SW_CIDEAUTOS/SW_CIDEAUTOS/Pago.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SW_CIDEAUTOS
+{
+    //Nombre txtNombre
+    //No de tarjeta txtNoTarjeta
+    //Codigo txtCodigo
+    public partial class Pago : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btEnviar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
